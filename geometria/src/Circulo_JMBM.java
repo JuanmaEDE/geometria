d1377cafab41f6169bf3dd7a16f115a93595eb1d
JuanmaEDE <juanmaclaseinformatica@gmail.com>
2023-02-19 20:48:40 +0100

    Siempre positivo

--- geometria/src/Circulo_JMBM.java
+++ geometria/src/Circulo_JMBM.java
@@ -1,23 +1,25 @@
 //primero
 public class Circulo_JMBM extends FiguraGeometrica_JMBM {
-	private double radio;
-	//Constante pi
-	static final double PI = 3.1416;
-	
-	public Circulo_JMBM(double r, String tipoFigura) {
-		super(tipoFigura);
-		radio = r;
-	}
+    private double radio;
+    //Constante pi
+    static final double PI = 3.1416;
 
-	@Override
-	public double area() {
-		return radio * radio * PI;
+    public Circulo_JMBM(double r, String tipoFigura) {
+        super(tipoFigura);
+        if (r < 0) {
+            radio = -r;
+        } else {
+            radio = r;
+        }
+    }
 
-	}
-	
-	@Override
-	public double perimetro() {
-		return radio * 2 * PI;
-	}
+    @Override
+    public double area() {
+        return radio * radio * PI;
+    }
 
+    @Override
+    public double perimetro() {
+        return radio * 2 * PI;
+    }
 }
