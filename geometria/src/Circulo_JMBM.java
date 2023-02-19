b7402b53f3603a0c4e05bc74d55b61be7099f77c
JuanmaEDE <juanmaclaseinformatica@gmail.com>
2023-02-19 20:22:17 +0100

    versión refactorizada

--- geometria/src/Circulo_JMBM.java
+++ geometria/src/Circulo_JMBM.java
@@ -1,10 +1,10 @@
 //primero
-public class Circulo extends FiguraGeometrica {
+public class Circulo_JMBM extends FiguraGeometrica_JMBM {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_JMBM(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_JMBM.java
+++ geometria/src/FiguraGeometrica_JMBM.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_JMBM {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_JMBM(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
